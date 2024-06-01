3ff9d7f7db8471356d1a702bfcbbfcb1398d32ea
arashjp8 <arashjafarpour723@gmail.com>
2024-06-01 15:07:54 +0330

    [checkpoint] - rotation algorithm added

--- GL1_basicShapes/main.cpp
+++ GL1_basicShapes/main.cpp
@@ -57,11 +57,18 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
+        glBegin(GL_QUADS);
         glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
         glVertex2f(0.5f, -0.5f);
+        glVertex2f(0.5f, 0.5f);
+        glVertex2f(-0.5f, 0.5f);
         glEnd();
+
+        // glBegin(GL_TRIANGLES);
+        // glVertex2f(-0.5f, -0.5f);
+        // glVertex2f(0.0f, 0.5f);
+        // glVertex2f(0.5f, -0.5f);
+        // glEnd();
 
         // Swap buffers
         glfwSwapBuffers(window);
